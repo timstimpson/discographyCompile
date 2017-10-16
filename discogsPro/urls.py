e4134c672d75bee35241cd2a7774e9048cd96b06
tim stimpson <timothy.stimpson@gmail.com>
2017-10-16 12:41:51 +0100

    removed spaces from  urls.py

--- discogsPro/urls.py
+++ discogsPro/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from discogsApp import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='home'),
-	url(r'^find_artist/$', views.find_artist, name='find_artist'),
+    url(r'^$', views.index, name='home'),
+    url(r'^find_artist/$', views.find_artist, name='find_artist'),
     url(r'^find_alias/$', views.find_alias, name='find_alias'),
     url(r'^confirm_alias/$', views.confirm_alias, name='confirm_alias'),
     url(r'^admin/', admin.site.urls),
